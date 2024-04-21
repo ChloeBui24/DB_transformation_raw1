e8a624d590c0fc19827644ac8cd2d7b5720b09f8
ChloeBui24 <157083711+ChloeBui24@users.noreply.github.com>
2024-04-20 22:29:43 -0700

    Add files via upload

--- clean1.sql
+++ clean1.sql
@@ -1,14 +1,9 @@
-
-INSERT INTO WALMART_STOCK(Timestamp, Open, High, Low, Close, Volume)
+INSERT INTO Amazon_SMA (Timestamp, SMA)
 SELECT
     key::TIMESTAMP AS Timestamp,
-    (value->>'1. open')::NUMERIC(10, 4) AS Open,
-    (value->>'2. high')::NUMERIC(10, 4) AS High,
-    (value->>'3. low')::NUMERIC(10, 4) AS Low,
-    (value->>'4. close')::NUMERIC(10, 4) AS Close,
-    (value->>'5. volume')::BIGINT AS Volume
+    (value->>'SMA')::NUMERIC(10, 4) AS SMA   
 FROM
-    RawData1,
-    jsonb_each(raw_json->'Time Series (60min)') AS timeseries(key, value)
+    RawData2,
+    jsonb_each(raw_json->'Technical Analysis: SMA') AS timeseries(key, value)
 ON CONFLICT (Timestamp)
 DO NOTHING;
